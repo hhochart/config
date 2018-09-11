4ae60f6f5cc41476449a78ce1b32edc085a47f29
Herve <h.hochart29@gmail.com>
2018-09-11 13:57:05 +0200

    :rocket: i3 launch & rsa key

--- .config/fish/fish/config.fish
+++ .config/fish/fish/config.fish
@@ -1,3 +1,6 @@
 alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
 set -g theme_display_date no
 source ~/.local/share/icons-in-terminal/icons.fish
+source ~/.config/fish/nvm-wrapper/nvm.fish
+ssh-add id_rsa # personnal github
+ssh-add rsa_work # professionnal gitlab
